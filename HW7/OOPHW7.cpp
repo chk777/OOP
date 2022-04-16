ea64cbdfedea8b6f8a18fc9fc03037b85d02b4b8
chk777 <93346990+chk777@users.noreply.github.com>
2022-04-16 19:44:43 +0300

    Update OOPHW7.cpp

--- HW7/OOPHW7.cpp
+++ HW7/OOPHW7.cpp
@@ -1,20 +1,109 @@
-﻿// OOPHW7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+#include<iosfwd>
+#include<windows.h>
+#include<memory>
+#include<ctime>
+#include"MyLib.h"
+#include"Part1.h"
+#include"Part2.h"
+#include"Part3.h"
 
-#include <iostream>
+using namespace std;
+using namespace myLib;
+
+void Part1()
+{
+	cout << "Work on Class Date:" << endl << endl;
+	time_t now = time(0);
+	tm currTime;
+	gmtime_s(&currTime, &now);
+
+	auto today = make_unique<Date>(currTime.tm_mday, currTime.tm_mon + 1, currTime.tm_year + 1900);
+	unique_ptr<Date> date;
+
+	cout << "Today date: " << endl;
+	cout << "Day: " << today.get()->getDay() << endl;
+	cout << "Month: " << today.get()->getMonth() << endl;
+	cout << "Year: " << today.get()->getYear() << endl << endl;
+	cout << *today << endl;
+
+	cout << "object today: " << (static_cast<bool>(today) ? "NOT null\n" : "null\n");
+	cout << "object date: " << (static_cast<bool>(date) ? "NOT null\n" : "null\n");
+	cout << "Move object today -> date" << endl;
+	date = move(today);
+	cout << "object today: " << (static_cast<bool>(today) ? "NOT null\n" : "null\n");
+	cout << "object date: " << (static_cast<bool>(date) ? "NOT null\n" : "null\n");
+
+}
+
+void Part2()
+{
+	cout << "Maximum date search:" << endl << endl;
+	auto date1 = make_shared<Date>(15, 4, 2022);
+	auto date2 = make_shared<Date>(16, 4, 202);
+	cout << "date1: " << *date1 << endl;
+	cout << "date2: " << *date2 << endl;
+	auto dateMax = maxDate(date1, date2);
+	cout << "dateMax: " << *dateMax << endl;
+	cout << "checking for the existence of objects:" << endl;
+	cout << "date1: " << *date1 << endl;
+	cout << "date2: " << *date2 << endl << endl;
+
+	cout << "Swap places date1 и date2: " << endl;
+	swapDate(date1, date2);
+	cout << "date1: " << *date1 << endl;
+	cout << "date2: " << *date2 << endl << endl;
+
+	cout << "check that the dateMax is correct: " << endl;
+	cout << "dateMax: " << *dateMax << endl;
+
+}
+
+void Part3()
+{
+	cout << "\t\tLets play Blackjack!" << endl << endl;
+	int numPlayers = 0;
+	while (numPlayers < 1 || numPlayers > 7)
+	{
+		cout << "How many players? (1 - 7): ";
+		numPlayers = getUserInput<uint16_t>();
+	}
+	vector<string> names;
+	names.reserve(numPlayers);
+	string name;
+	for (int i = 0; i < numPlayers; ++i)
+	{
+		cout << "Enter name " << i + 1 << " player: ";
+		name = getUserInputTxt();
+		names.push_back(name);
+	}
+	cout << endl;
+	// процес игры
+	Game aGame(names);
+	char again = 'y';
+	while (again != 'n' && again != 'N')
+	{
+		aGame.Play();
+		cout << "\nDo you want to play again? (Y/N): ";
+		cin >> again;
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	unsigned short taskNumber = 0;
+	while (true)
+	{
+		taskNumber = getUserSelectedTask(3);
+		switch (taskNumber)
+		{
+		case 1: Part1();
+			break;
+		case 2: Part2();
+			break;
+		case 3: Part3();
+			break;
+		}
+		cout << endl;
+	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
